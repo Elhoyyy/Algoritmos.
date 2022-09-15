af0daa29d54e3135467553b47170e14ecb1cabdc
Elhoyyy <100317972+Elhoyyy@users.noreply.github.com>
2022-09-15 16:34:57 +0200

    Update p1.c

--- p1.c
+++ p1.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include <math.h>
+void test();
+int main (){
+    test();
+    return 0;
+}
 
 int fib1(int n) {
-    if (n > 2) {
+    if (n < 2) {
         return n;
     } else {
-        return fib1(n - 1) + fib1(n - 2);
+        return (fib1(n - 1) + fib1(n - 2));
     }
 }
 
 int fib2(int n){
-    int i=1, j=2;
+    int i=1, j=0;
     for(int k=1; k<n; k++){
         j=i+j;
         i=j-i;
@@ -23,21 +28,26 @@ int fib3(int n) {
     while (n > 0) {
         if (n % 2 == 1) {
             t = j * h;
-            h = 2 * k * h + t;
-            k = pow(k, 2) + t;
-            n = n / 2;
+            j=i*h+j*k+t;
+            i=i*k+t;
         }
+        t=pow(h,2);
+        h = 2 * k * h + t;
+        k = pow(k, 2) + t;
+        n = n / 2;
+
     }
     return j;
 }
-
 void test(){
     int n=10;
-    for(int i =0;i<n;i++)       {
-            printf("%d\t",i+1);
-            printf("%d\t",fib1(i+1));
-            printf("%d\t",fib2(i+1));
-            printf("%d\n",fib3(i+1));
+    printf("\n");
+    for(int i =0;i<n;i++){
 
-        }
+        printf("%d\t",i+1);
+        printf("%d\t",fib1(i+1));
+        printf("%d\t",fib2(i+2));
+        printf("%d\n",fib3(i+1));
+
     }
+}
