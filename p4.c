73d2fba389175ee7b5076a40b2a3926c107fc838
Elhoyyy <100317972+Elhoyyy@users.noreply.github.com>
2022-11-16 21:28:40 +0100

    Update p4.c

--- p4.c
+++ p4.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <sys/time.h>
 #include <stdlib.h>
-
+#include <math.h>
 #define TAM 512000
 
 typedef struct {
@@ -13,6 +13,13 @@ typedef struct {
 void ord_monticulo(int vec[], int n);
 void inicializar_semilla();
 void aleatorio(int v [], int n);
+void ascendente(int v [], int n);
+void descendente(int v [], int n);
+
+void print_result_ascendente(int n);
+void print_result_aleatorio(int n);
+void print_result_descendente(int n);
+
 void intercambiar(int vec[], int a, int b);
 void copiarvectores(const int a[], int b[], int tamano);
 void hundir (monticulo *m, int i);
@@ -24,15 +31,68 @@ int eliminar_mayor(monticulo *m);
 void inicializar_semilla() {
     srand(time(NULL));
 }
+
+double microsegundos() {
+    struct timeval t;
+    if (gettimeofday(&t, NULL) < 0 )
+        return 0.0;
+    return (t.tv_usec + t.tv_sec * 1000000.0);
+}
 void aleatorio(int v [], int n) {/* se generan números pseudoaleatorio entre -n y +n */
     int i, m=2*n+1;
     for (i=0; i < n; i++)
         v[i] = (rand() % m) - n;
 }
+void ascendente(int v [], int n) {
+    int i;
+    for (i=0; i < n; i++)
+        v[i] = i;
+}
+void descendente(int v[], int n){
+    int i,j=0;
+    for(i=n; i>0; i--){
+        v[j]=i;
+        j++;
+    }
+}
 
 int main (){
+    int n1=500, n2=500, n3=500, i;
     inicializar_semilla();
     test();
+
+    printf("TABLAS:\n\n");
+    printf("ALEATORIA:\n\n");
+
+    printf("\t      n\t        t(n)\t    t(n)/f(n)\t   t(n)/g(n)\t  t(n)/h(n)\t\n");
+    for(i=0; i<7;i++){
+        print_result_descendente(n1);
+        n1=n1*2;
+
+    }
+
+    printf("\n");
+    printf("ASCENDENTE:\n\n");
+
+    printf("\t      n\t        t(n)\t    t(n)/f(n)\t   t(n)/g(n)\t  t(n)/h(n)\t\n");
+    for (i = 0; i<7; i++){
+        print_result_ascendente(n2);
+        n2=n2*2;
+
+    }
+
+    printf("\n");
+    printf("DESCENDENTE:\n\n");
+
+    printf("\t      n\t        t(n)\t    t(n)/f(n)\t   t(n)/g(n)\t  t(n)/h(n)\t\n");
+    for( i = 0; i<7; i++){
+        print_result_aleatorio(n3);
+        n3=n3*2;
+
+    }
+
+
+    return 0;
 }
 
 
@@ -104,6 +164,119 @@ void hundir (monticulo *m, int i) {
 
 }
 
+void print_result_aleatorio(int n){
+    double t1, t2,ta, tb, t, x, y, z;
+    int vector[n], k=1000, i;
+    aleatorio(vector, n);
+    t1 = microsegundos();
+    ord_monticulo(vector, n);
+    t2 = microsegundos();
+    t = t2 - t1;
+    if (t < 500) {
+        ta = microsegundos();
+        for ( i = 0; i < k; i++) {
+            aleatorio(vector, n);
+            ord_monticulo(vector, n);
+        }
+        tb = microsegundos();
+        t1 = (tb - ta);
+        ta = microsegundos();
+        for ( i = 0; i < k; i++) {
+            aleatorio(vector, n);
+        }
+        tb = microsegundos();
+        t2 = tb - ta;
+        t = (t1 - t2) / k;
+        printf("(*)");
+
+    } else {
+        printf("   ");
+    }
+        x = t /n;
+        y = t / pow(n, 1.1);
+        z = t / pow(n, 1.2);
+
+    printf("%12d%15.3f%15.6f%15.6f%15.6f\n", n, t, x, y, z);
+
+}
+
+
+
+
+
+void print_result_ascendente(int n) {
+    double t1, t2,ta, tb, t, x, y, z;
+    int vector[n], k=1000, i;
+    ascendente(vector, n);
+    t1 = microsegundos();
+    ord_monticulo(vector,n);
+    t2 = microsegundos();
+    t = t2 - t1;
+    if (t < 500) {
+        ta = microsegundos();
+        for ( i = 0; i < k; i++) {
+            ascendente(vector, n);
+            ord_monticulo(vector,n);
+        }
+        tb = microsegundos();
+        t1 = (tb - ta);
+        ta = microsegundos();
+        for ( i = 0; i < k; i++) {
+            ascendente(vector, n);
+        }
+        tb = microsegundos();
+        t2 = tb - ta;
+        t = (t1 - t2) / k;
+        printf("(*)");
+
+    } else {
+        printf("   ");
+    }
+
+    x = t / pow(n, 0.8);
+    y = t /n ;
+    z = t / pow(n, 1.2);
+    printf("%12d%15.3f%15.6f%15.6f%15.6f\n", n, t, x, y, z);
+
+}
+
+
+void print_result_descendente(int n){
+    double t1, t2,ta, tb, t, x, y, z;
+    int vector[n], k=1000, i;
+    descendente(vector, n);
+    t1 = microsegundos();
+    ord_monticulo(vector,n);
+    t2 = microsegundos();
+    t = t2 - t1;
+    if (t < 500) {
+        ta = microsegundos();
+        for ( i = 0; i < k; i++) {
+            descendente(vector, n);
+            ord_monticulo(vector,n);
+        }
+        tb = microsegundos();
+        t1 = (tb - ta);
+        ta = microsegundos();
+        for ( i = 0; i < k; i++) {
+            descendente(vector, n);
+        }
+        tb = microsegundos();
+        t2 = tb - ta;
+        t = (t1 - t2) / k;
+        printf("(*)");
+
+    } else {
+        printf("   ");
+    }
+    x = t / pow(n, 1.8);
+    y = t / pow(n, 2);
+    z = t / pow(n, 2.2);
+
+    printf("%12d%15.3f%15.6f%15.6f%15.6f\n", n, t, x, y, z);
+
+}
+
 //Pequeñas funciones auxiliares a otras.
 
 void intercambiar(int vec[], int a, int b){
@@ -122,11 +295,4 @@ void copiarvectores(const int a[], int b[], int tamano) {
     for (int i = 0; i < tamano; i++) {
         printf("%i", b[i]);
     }
-}
-
-double microsegundos() {
-    struct timeval t;
-    if (gettimeofday(&t, NULL) < 0 )
-        return 0.0;
-    return (t.tv_usec + t.tv_sec * 1000000.0);
 }
