542aec010b94b3c2d9187c4a5d8531f699c8c338
Elhoyyy <100317972+Elhoyyy@users.noreply.github.com>
2022-12-25 16:38:16 +0100

    Update vairiantep2mediana.c

--- vairiantep2mediana.c
+++ vairiantep2mediana.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 
 /*
 TITULO: Algoritmos Práctica 1
@@ -23,9 +25,9 @@ void Medianatres (int v[], int i, int j);
 void print_result_aleatorio(int n);
 void print_result_ascendente(int n);
 void print_result_descendente(int n);
-void ord_rapida(int v [], int n);
+
 void intercambiar(int vec[], int a, int b);
-void rapida_aux(int v[ ], int izq, int der);
+
 void Quicksort ( int v[], int n);
 
 
@@ -46,36 +48,36 @@ void ord_ins(int v[], int n){
 int main(){
     int  n1,n2,n3,i;
     inicializar_semilla();
-        test();
-        n1=500,n2=500,n3=500;
+    test();
+    n1=500,n2=500,n3=500;
 
-        printf("TABLAS:\n\n");
-        printf("ALEATORIA:\n\n");
-
-
-        printf("\t      n\t        t(n)\t    t(n)/f(n)\t   t(n)/g(n)\t  t(n)/h(n)\t\n");
-        for (i = 0; i < 7; i++) {
-            print_result_aleatorio(n1);
-            n1 = n1 * 2;
-        }
-        printf("\n");
-        printf("ASCENDENTE:\n\n");
+    printf("TABLAS:\n\n");
+    printf("ALEATORIA:\n\n");
 
 
-        printf("\t      n\t        t(n)\t    t(n)/f(n)\t   t(n)/g(n)\t  t(n)/h(n)\t\n");
-        for (i = 0; i < 7; i++) {
-            print_result_ascendente(n2);
-            n2 = n2 * 2;
-        }
-        printf("\n");
-        printf("DESCENDENTE:\n\n");
+    printf("\t      n\t        t(n)\t    t(n)/f(n)\t   t(n)/g(n)\t  t(n)/h(n)\t\n");
+    for (i = 0; i < 7; i++) {
+        print_result_aleatorio(n1);
+        n1 = n1 * 2;
+    }
+    printf("\n");
+    printf("ASCENDENTE:\n\n");
 
 
-        printf("\t      n\t         t(n)\t    t(n)/f(n)\t   t(n)/g(n)\t  t(n)/h(n)\t\n");
-        for (i = 0; i < 7; i++) {
-            print_result_descendente(n3);
-            n3 = n3 * 2;
-        }
+    printf("\t      n\t        t(n)\t    t(n)/f(n)\t   t(n)/g(n)\t  t(n)/h(n)\t\n");
+    for (i = 0; i < 7; i++) {
+        print_result_ascendente(n2);
+        n2 = n2 * 2;
+    }
+    printf("\n");
+    printf("DESCENDENTE:\n\n");
+
+
+    printf("\t      n\t         t(n)\t    t(n)/f(n)\t   t(n)/g(n)\t  t(n)/h(n)\t\n");
+    for (i = 0; i < 7; i++) {
+        print_result_descendente(n3);
+        n3 = n3 * 2;
+    }
 
     return 0;
 }
@@ -83,15 +85,15 @@ int main(){
 void Medianatres (int v[], int i, int j){
     int centro = (i+j) /2;
     if( v[i]>v[centro]){
-     intercambiar (v, i,centro);
-        }
+        intercambiar (v, i,centro);
+    }
     if (v[i]>v[j]){
         intercambiar(v,i,j);
     }
     if (v[centro]>v[j]) {
         intercambiar(v, centro, j);
     }
-    intercambiar(v, centro, i-1);
+    intercambiar(v, centro, j-1);
 }
 
 void inicializar_semilla() {
@@ -117,9 +119,9 @@ void descendente(int v[], int n){
 void test () {
     int v1[20], v2[17], i;
     descendente(v1, 20);
-        printf("\nORDENACION POR QUICKSORT:\n");
+    printf("\nORDENACION POR QUICKSORT:\n");
 
-        printf("\nOrdenacion por quicksort con inicializacion descendete:\n");
+    printf("\nOrdenacion por quicksort con inicializacion descendete:\n");
 
 
     for (i = 0; i < 20; i++) {
@@ -129,7 +131,7 @@ void test () {
     printf("\n");
     printf("Vector ordenado: \n");
 
-        Quicksort(v1, 20);
+    Quicksort(v1, 20);
 
 
     for (i = 0; i < 20; i++) {
@@ -139,7 +141,7 @@ void test () {
     printf("\n\n");
     aleatorio(v2, 17);
 
-      printf("\nOrdenacion rapida con inicializacion aleatoria:\n");
+    printf("\nOrdenacion rapida con inicializacion aleatoria:\n");
 
 
     for (i = 0; i < 17; i++) {
@@ -148,7 +150,7 @@ void test () {
 
     printf("\n");
 
-        Quicksort(v2, 17);
+    Quicksort(v2, 17);
 
 
     printf("Vector ordenado.\n ");
@@ -201,10 +203,9 @@ void print_result_aleatorio(int n){
         printf("   ");
     }
 
-        x = t / pow(n, 1.8);
-        y = t / pow(n, 2);
-        z = t / pow(n, 2.2);
-
+    x = t / pow(n, 1.8);
+    y = t / pow(n, 2);
+    z = t / pow(n, 2.2);
     printf("%12d%15.3f%15.6f%15.6f%15.6f\n", n, t, x, y, z);
 
 }
@@ -244,10 +245,10 @@ void print_result_ascendente(int n) {
     } else {
         printf("   ");
     }
-        x = t / pow(n, 0.8);
-        y = t /n ;
-        z = t / pow(n, 1.2);
 
+    x = t / pow(n, 0.8);
+    y = t /n ;
+    z = t / pow(n, 1.2);
     printf("%12d%15.3f%15.6f%15.6f%15.6f\n", n, t, x, y, z);
 
 }
@@ -283,70 +284,69 @@ void print_result_descendente(int n ){
     } else {
         printf("   ");
     }
-        x = t / pow(n, 1.8);
-        y = t / pow(n, 2);
-        z = t / pow(n, 2.2);
+
+    x = t / pow(n, 1.8);
+    y = t / pow(n, 2);
+    z = t / pow(n, 2.2);
 
     printf("%12d%15.3f%15.6f%15.6f%15.6f\n", n, t, x, y, z);
 
 }
 
-void rapida_aux(int v[ ], int izq, int der){
-    int x,pivote,i,j;
+void Qsort ( int v[], int izq, int der ){
+    int pivote=0, i=0, j=0 ;
+    if ( (izq + UMBRAL) <= der ) {
+        Medianatres(v, izq, der);
+        pivote = v[izq];
+        i=izq;
+        j= der;
+        do {
+            do {
+                i ++;
+            } while (v[i] < pivote);
 
-    if ((izq+UMBRAL)<= der ){
-        x=rand() % (der-izq+1)+izq;
-        pivote=v[x];
+            do {
+                j --;
+            } while (v[j] > pivote);
 
-        intercambiar(v, izq, x);
-        i=izq+1;
-        j=der;
-        while(i<=j) {
-            while (i <= der && v[i] < pivote) {
-                i++;
-            }
-            while (v[j] > pivote) {
-                j--;
-            }
-            if (i <= j) {
-                intercambiar(v, i, j);
-                i++;
-                j--;
-            }
+            intercambiar(v, i , j);
         }
-        intercambiar(v, izq, j);
-        rapida_aux(v, izq, j-1);
-        rapida_aux(v, j+1, der);
-
+        while ( j > i);
+        intercambiar(v, i , j);
+        intercambiar(v, izq , j);
+        Qsort(v, izq, j-1);
+        Qsort(v, j+1, der);
     }
 }
-
+/*
+ * SEGÚN EL PSEUDOCÓDIGO SERÍA ASÍ, PERO NO FUNCIONA:
+ *
 void Qsort ( int v[], int izq, int der ){
     int pivote, i, j ;
-        if ( (izq + UMBRAL) <= der ) {
-            Medianatres(v, izq, der);
-            pivote = v[der-1];
-            i=izq;
-            j= der-1;
+    if ( (izq + UMBRAL) <= der ) {
+        Medianatres(v, izq, der);
+        pivote = v[der-1];
+        i=izq;
+        j= der-1;
+        do {
             do {
-                do {
-                    i ++;
-                } while (v[i] >= pivote);
+                i ++;
+            } while (v[i] >= pivote);
 
-                do {
-                    j --;
-                } while (v[j] <= pivote);
+            do {
+                j --;
+            } while (v[j] <= pivote);
 
-                intercambiar(v, i , j);
-            }
-                while ( j <= i);
             intercambiar(v, i , j);
-            intercambiar(v, i , der-1);
-            Qsort(v, izq, i-1);
-            Qsort(v, i+1, der);
         }
+        while ( j <= i);
+        intercambiar(v, i , j);
+        intercambiar(v, i , der-1);
+        Qsort(v, izq, i-1);
+        Qsort(v, i+1, der);
+    }
 }
-
+ */
 void intercambiar(int vec[], int a, int b){
     int aux;
     aux=vec[a];
@@ -354,14 +354,10 @@ void intercambiar(int vec[], int a, int b){
     vec[b]=aux;
 
 }
-void ord_rapida(int v [], int n) {
-    rapida_aux(v, 0, n-1);
-    if (UMBRAL > 1)
-        ord_ins(v, n);
-}
-
 void Quicksort ( int v[], int n){
     Qsort(v, 0, n-1);
     if ( UMBRAL>1)
-    ord_ins(v, n);
+        ord_ins(v, n);
 }
+
+
